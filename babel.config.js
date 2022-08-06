ad4c3d7202371047567db00e0f9cb08aba76136b
free <220201752@seu.edu.cn>
2022-08-06 16:11:44 +0800

    feat(api): add jsError and resouceError and promiseError

--- babel.config.js
+++ babel.config.js
@@ -2,7 +2,7 @@ module.exports = {
   presets: [
     [
       '@babel/preset-env',
-      { targets: { browsers: ['last 2 versions', 'not ie < 11'] }, modules: false }
+      { targets: { browsers: ['last 2 versions', 'not ie < 11'] }, modules: false, node: true }
     ],
     '@babel/preset-typescript'
   ],
